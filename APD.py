4709082e006a8b9f2d518968ad97fd06d4a63020
Sriharish Pingali <sriharish.pingali@gmail.com>
2018-05-24 01:23:16 -0400

    Add files via upload

--- APD.py
+++ APD.py
@@ -9,6 +9,11 @@ def createdata(src):
     data = open('data','w+')
     source = requests.get(src)
     soup = BeautifulSoup(source.text,"lxml")
+    div = soup.find("div",id="all_team_stats")
+    timesearch= re.search('(?P<mins1>\d\d):(?P<secs1>\d\d).*(?P<mins2>\d\d):(?P<secs2>\d\d)',div.prettify())
+    if timesearch:
+        data.write("Total time: %d\n"%(60*(int)(timesearch.group('mins1'))+(int)(timesearch.group('secs1'))+60*(int)(timesearch.group('mins2'))+(int)(timesearch.group('secs2'))))
+    
     rows = soup.find("table",id="scoring").tbody.find_all("tr")
     for row in rows:
         line = ""
@@ -19,7 +24,7 @@ def createdata(src):
     data.close()
 
 #given a gamelog txt file, reads it and calculates the in-game average point differential
-def apd(logfilename):
+def apd(logfilename,playoff):
     fName = open(logfilename,'r')
     numscores=0
     scorediff={}
@@ -27,13 +32,17 @@ def apd(logfilename):
     scorediff[numscores]=0
     time[numscores]=0
     num = 15
-    ottime = 10 #15 in playoffs?
-    
+    ottime = 10
+    if(playoff==1):
+        ottime = 15
+    totaltime=3600
     for line in fName:
-        #totaltimesearch = re.search('Total time: (?P<mins>\d+):(?P<secs>\d\d)',line)
+        totaltimesearch = re.search('Total time: (?P<time>\d+)',line)
         otsearch = re.search('OT(?P<ot>\d*) (?P<mins>\d+):(?P<secs>\d\d).* (?P<away>\d+) (?P<home>\d+)',line)
         quartersearch = re.search('(?P<quarter>\d) (?P<mins>\d+):(?P<secs>\d\d).* (?P<away>\d+) (?P<home>\d+)',line)
         scoresearch=re.search('(?P<mins>\d+):(?P<secs>\d\d).* (?P<away>\d+) (?P<home>\d+)',line)
+        if totaltimesearch:
+            totaltime=(int)(totaltimesearch.group('time'))
         if otsearch:
             if otsearch.group('ot'):
                 num = 60+(int)(otsearch.group('ot'))*ottime
@@ -58,17 +67,17 @@ def apd(logfilename):
             scorediff[numscores]=(int)(scoresearch.group('home'))-(int)(scoresearch.group('away'))
             time[numscores]=60*num-60*(int)(scoresearch.group('mins'))-(int)(scoresearch.group('secs'))
     fName.close()
-    #for x in range(0,numscores+1):
-        #print "At %d:%d, the score differential is %d"%(mins[x],secs[x],scorediff[x])
     totalpd=0.0
     for x in range (0, numscores):
         deltatime = time[x+1]-time[x]
         totalpd += deltatime*scorediff[x]
-    totalpd+=(3600-time[numscores])*scorediff[numscores]
-    #totalpd+=(totaltime-time[numscores])*scorediff[numscores]
-    apd = totalpd/3600.0
-    print "APD: %f"%apd
+    totalpd+=(totaltime-time[numscores])*scorediff[numscores]
+    apd = totalpd/totaltime
+    return apd
     
 if __name__ == '__main__':
-    createdata(sys.argv[1])
-    apd('data')
+    if (int)(sys.argv[1]):
+        createdata(sys.argv[2])
+        print(apd('data',(int)(sys.argv[3])))
+    else:
+        print(apd(sys.argv[2],(int)(sys.argv[3])))
